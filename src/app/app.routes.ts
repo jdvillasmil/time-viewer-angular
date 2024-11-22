73ad5cbca9296b311abedd222067b091bb557281
jdvillasmil <jdvillasmil007@gmail.com>
2024-11-22 17:06:49 -0400

    feat: integración del reloj binario en el menú y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MenuRelojesComponent } from './menu-relojes/menu-relojes.component';
 import { RelojDigitalComponent } from './reloj-digital/reloj-digital.component';
+import { RelojAnalogicoComponent } from './reloj-analogico/reloj-analogico.component'; // Importa el Reloj Analógico
+import { RelojBinarioComponent } from './reloj-binario/reloj-binario.component'; // Importa el Reloj Binario
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -10,5 +12,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'menu-relojes', component: MenuRelojesComponent },
   { path: 'reloj-digital', component: RelojDigitalComponent },
+  { path: 'reloj-analogico', component: RelojAnalogicoComponent }, // Añade la ruta para el Reloj Analógico
+  { path: 'reloj-binario', component: RelojBinarioComponent }, // Añade la ruta para el Reloj Binario
   { path: '**', redirectTo: 'login' }, // Ruta para manejar errores
 ];
--- src/app/menu-relojes/menu-relojes.component.ts
+++ src/app/menu-relojes/menu-relojes.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { RelojDigitalComponent } from '../reloj-digital/reloj-digital.component';
 import { RelojAnalogicoComponent } from '../reloj-analogico/reloj-analogico.component';
+import { RelojBinarioComponent } from '../reloj-binario/reloj-binario.component'; // Importa el Reloj Binario
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-menu-relojes',
   standalone: true,
   templateUrl: './menu-relojes.component.html',
   styleUrls: ['./menu-relojes.component.css'],
-  imports: [CommonModule, RelojDigitalComponent, RelojAnalogicoComponent],
+  imports: [CommonModule, RelojDigitalComponent, RelojAnalogicoComponent, RelojBinarioComponent], // Añade el Reloj Binario a los imports
 })
 export class MenuRelojesComponent {
-  selectedClock: string = '';
+  selectedClock: string = 'reloj-digital';
+
+  constructor(private cdr: ChangeDetectorRef) {}
 
   selectClock(clock: string) {
     this.selectedClock = clock;
+    this.cdr.detectChanges(); // Fuerza la detección de cambios
   }
 }
