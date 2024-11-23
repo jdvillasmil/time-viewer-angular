8f57acf04b59763032a42b2fa47eb99b082d65ea
jdvillasmil <jdvillasmil007@gmail.com>
2024-11-22 20:16:10 -0400

    chore: Actualizar menu-relojes.component para incluir nuevos relojes en el menú

--- src/app/menu-relojes/menu-relojes.component.ts
+++ src/app/menu-relojes/menu-relojes.component.ts
@@ -1,23 +1,30 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RelojDigitalComponent } from '../reloj-digital/reloj-digital.component';
 import { RelojAnalogicoComponent } from '../reloj-analogico/reloj-analogico.component';
-import { RelojBinarioComponent } from '../reloj-binario/reloj-binario.component'; // Importa el Reloj Binario
-import { CommonModule } from "@angular/common";
+import { RelojBinarioComponent } from '../reloj-binario/reloj-binario.component';
+import { RelojHexadecimalComponent } from '../reloj-hexadecimal/reloj-hexadecimal.component';
+import { RelojPalabrasComponent } from '../reloj-palabras/reloj-palabras.component';
 
 @Component({
   selector: 'app-menu-relojes',
   standalone: true,
+  imports: [CommonModule, RelojDigitalComponent, RelojAnalogicoComponent, RelojBinarioComponent, RelojHexadecimalComponent, RelojPalabrasComponent],
   templateUrl: './menu-relojes.component.html',
-  styleUrls: ['./menu-relojes.component.css'],
-  imports: [CommonModule, RelojDigitalComponent, RelojAnalogicoComponent, RelojBinarioComponent], // Añade el Reloj Binario a los imports
+  styleUrls: ['./menu-relojes.component.css']
 })
 export class MenuRelojesComponent {
   selectedClock: string = 'reloj-digital';
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  relojes = [
+    { key: 'reloj-digital', name: 'Reloj Digital' },
+    { key: 'reloj-analogico', name: 'Reloj Analógico' },
+    { key: 'reloj-binario', name: 'Reloj Binario' },
+    { key: 'reloj-hexadecimal', name: 'Reloj Hexadecimal' },
+    {key: 'reloj-palabras', name: 'Reloj Palabras' },
+  ];
 
   selectClock(clock: string) {
     this.selectedClock = clock;
-    this.cdr.detectChanges(); // Fuerza la detección de cambios
   }
 }
